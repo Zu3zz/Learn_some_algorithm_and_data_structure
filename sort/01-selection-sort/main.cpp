fe4b11f54961d3a4e5ff4f99626ce76a66b29236
Zu3zz <zthzththth@163.com>
2019-06-30 23:48:43 +0800

    update selection sort template

--- sort/01-selection-sort/main.cpp
+++ sort/01-selection-sort/main.cpp
@@ -18,10 +18,10 @@ void selectionSort( int arr[], int n) {
 
 int main() {
     int a[10] = {10,9,8,7,6,5,4,3,2,1};
-    selectionSort(a, 10)
+    selectionSort(a, 10);
     for(int i =0;i<10;i++)
         cout<<a[i]<<" ";
     cout<<endl;
-    return 0
+    return 0;
 }
 
---
+++ sort/02-selection-sort-template/main.cpp
@@ -0,0 +1,57 @@
+//
+// Created by 郑天航 on 2019-06-30.
+//
+#include <iostream>
+#include "Student.h"
+
+using namespace std;
+
+template<typename T>
+void selectionSort(T arr[], int n) {
+
+    for (int i = 0; i < n; i++) {
+
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+
+    // 测试模板函数，传入整型数组
+    int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    selectionSort(a, 10);
+    for (int i = 0; i < 10; i++)
+        cout << a[i] << " ";
+    cout << endl;
+
+    // 测试模板函数，传入浮点数数组
+    float b[4] = {4.4, 3.3, 2.2, 1.1};
+    selectionSort(b, 4);
+    for (int i = 0; i < 4; i++)
+        cout << b[i] << " ";
+    cout << endl;
+
+    // 测试模板函数，传入字符串数组
+    string c[4] = {"D", "C", "B", "A"};
+    selectionSort(c, 4);
+    for (int i = 0; i < 4; i++)
+        cout << c[i] << " ";
+    cout << endl;
+
+    // 测试模板函数，传入自定义结构体Student数组
+    Student d[4] = {{"D", 90},
+                    {"C", 100},
+                    {"B", 95},
+                    {"A", 95}};
+    selectionSort(d, 4);
+    for (int i = 0; i < 4; i++)
+        cout << d[i];
+    cout << endl;
+
+    return 0;
+}
