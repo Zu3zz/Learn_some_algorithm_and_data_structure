c5953103c30389f9c57010c9d925afe822d54040
Zu3zz <zthzththth@163.com>
2019-07-01 12:40:38 +0800

    update SortTestHelper

---
+++ sort/03-selection-sort-generate-test/SortTestHelper.h
@@ -0,0 +1,36 @@
+//
+// created by 3zz.
+//
+#ifndef INC_03_SELECTION_SORT_GENERATE_TEST_CASES_SORTTESTHELPER_H
+#define INC_03_SELECTION_SORT_GENERATE_TEST_CASES_SORTTESTHELPER_H
+
+#include <iostream>
+#include <ctime>
+#include <cassert>
+using namespace std;
+
+namespace SortTestHelper
+{
+// 生成有n个元素的随机数组， 每个元素的随机范围为[rangeL, rangeR]
+int *generageRandomArray(int n, int rangeL, int rangeR)
+{
+  assert(rangeL <= rangeR);
+  int *arr = new int[n];
+  srand(time(NULL));
+  for (int i = 0; i < n; i++)
+  {
+    arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+  }
+  return arr;
+}
+template <typename T>
+
+void printArray(T arr[], int n)
+{
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+  cout << endl;
+  return;
+}
+};     // namespace SortTestHelper
+#endif //INC_03_SELECTION_SORT_GENERATE_TEST_CASES_SORTTESTHELPER_H
