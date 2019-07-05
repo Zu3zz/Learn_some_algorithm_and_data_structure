fc46ca5d2537b8e1eb69bc4359abe4814c5debc1
Zu3zz <zthzththth@163.com>
2019-07-05 15:13:34 +0800

    update two way quick sort

--- README.md
+++ README.md
@@ -22,22 +22,39 @@
     > ![插入排序改进](./static/insertion-sort-advanced.png)
 
   - 冒泡排序(Bubble Sort)
+
   - 希尔排序(Shell Sort)
+
   - 归并排序(Merge Sort)复杂度 O(n\*logn)
+
     > 先不断的二分
     > ![归并排序](./static/merge-sort.png)
     > 归并过程
     > ![归并排序](./static/merge-sort-detail.png)
+
   - 归并排序自底向上版(Merge Sort Buttom Up)
+
     > 不先对数组进行二分 直接以 1、2、4 这样大小循环合并直至数组全部合并完成
+
   - 快速排序(Quick Sort)
+
     > 先选中一个元素 然后找到它应该所在的位置 然后继续递归
     > ![快速排序](./static/quick-sort.png)
+
     - 第一部分 partition
       > 先选中一个元素 然后找到它应该所在的位置 然后继续递归
       > ![partition](./static/quick-sort-partition.png)
-      > 如果此时 e > v，那么e直接加入到紫色的数组中  
-      > 如果此时 e < v, 那么直接将e与j所指向的位置交换即可 之后j++
+      > 如果此时 e > v，那么 e 直接加入到紫色的数组中  
+      > 如果此时 e < v, 那么直接将 e 与 j 所指向的位置交换即可 之后 j++
       > ![partition-1](./static/quick-sort-partition-1.png)
-      > 最后所有元素分类完毕之后 将l与j所在位置的元素交换一下即可
+      > 最后所有元素分类完毕之后 将 l 与 j 所在位置的元素交换一下即可
       > ![partition-2](./static/quick-sort-partition-2.png)
+
+  - 快速排序存在的两点问题
+    1. 对于近乎有序的数组,快速排序会退化到复杂度O(n^2),此时解决办法就是随机选取一个初始值 然后与第一个需要定位的元素 l 交换 这样操作 快速排序的期望复杂度 EO(Nlogn)
+    2. 对于一个数量庞大但是所有元素都在一个很小区间的数组,快速排序依旧会退化到复杂度O(n^2),因为会导致大量的不平衡数组出现
+      > ![快速排序存在问题](./static/quick-sort-problem.png)
+
+  - 此时 使用双路快排
+    > 主要在partition过程中进行了更加细化的操作
+    ![双路快排](./static/quick-sort-two-way.png)
---
+++ sort-advanced/06-quick-sort-two-way/main.cpp
@@ -0,0 +1,145 @@
+//
+// created by 3zz.
+//
+#include <iostream>
+#include <algorithm>
+#include "SortTestHelper.h"
+#include "MergeSort.h"
+#include "InsertionSort.h"
+
+using namespace std;
+
+// 对arr[l...r]部分进行partition操作
+// 返回p,使得[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
+template <typename T>
+int __partition(T arr[], int l, int r)
+{
+  // 挑选一个随机数作为锚定
+  swap(arr[l], arr[rand() % (r - l + 1) + l]);
+  T v = arr[l];
+  // arr[l+1...j] < v ; arr[j+1...i] > v
+  int j = l;
+  for (int i = l + 1; i <= r; i++)
+  {
+    if (arr[i] < v)
+    {
+      // swap(arr[++j], arr[i])
+      swap(arr[j + 1], arr[i]);
+      j++;
+    }
+  }
+  swap(arr[l], arr[j]);
+  return j;
+}
+
+// 双路快速排序的partition
+// 返回p, 使得arr[l...p-1] <= arr[p] ; arr[p+1...r] >= arr[p]
+// 双路快排处理的元素正好等于arr[p]的时候要注意，详见下面的注释：）
+template <typename T>
+int __partition2(T arr[], int l, int r)
+{
+  // 选择一个随机数为锚定
+  swap(arr[l], arr[rand() % (r - l + 1) + l]);
+  T v = arr[l];
+  // arr[l+1...i) <= v; arr(j...r] >= v
+  int i = l + 1, j = r;
+  while (true)
+  {
+    while (i <= r && arr[i] < v)
+      i++;
+    while (j >= l + 1 && arr[j] > v)
+      j--;
+    if (i > j)
+      break;
+    swap(arr[i], arr[j]);
+    i++;
+    j--;
+  }
+  swap(arr[l], arr[j]);
+  return j;
+}
+
+// 对arr[l...r]部分进行快速排序
+template <typename T>
+void __quickSort(T arr[], int l, int r)
+{
+  // if (l >= r)
+  // return;
+  if (r - l <= 15)
+  {
+    insertionSort(arr, l, r);
+    return;
+  }
+  int p = __partition2(arr, l, r);
+  __quickSort(arr, l, p - 1);
+  __quickSort(arr, p + 1, r);
+}
+
+template <typename T>
+void quickSort(T arr[], int n)
+{
+  srand(time(NULL));
+  __quickSort(arr, 0, n - 1);
+}
+
+// 比较Merge Sort和Quick Sort两种排序算法的性能效率
+// 两种排序算法虽然都是O(nlogn)级别的, 但是Quick Sort算法有常数级的优势
+// Quick Sort要比Merge Sort快, 即使我们对Merge Sort进行了优化
+int main()
+{
+
+  int n = 1000000;
+
+  // 测试1 一般性测试
+  cout << "Test for random array, size = " << n << ", random range [0, " << n << "]" << endl;
+  int *arr1 = SortTestHelper::generateRandomArray(n, 0, n);
+  int *arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+  SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
+  SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+
+  delete[] arr1;
+  delete[] arr2;
+
+  cout << endl;
+  // 测试2 测试近乎有序的数组
+  // 但是对于近乎有序的数组, 我们的快速排序算法退化成了O(n^2)级别的算法
+  // 思考一下为什么对于近乎有序的数组, 快排退化成了O(n^2)的算法? :)
+  //    int swapTimes = 100;
+  //    cout<<"Test for nearly ordered array, size = "<<n<<", swap time = "<<swapTimes<<endl;
+  //    arr1 = SortTestHelper::generateNearlyOrderedArray(n,swapTimes);
+  //    arr2 = SortTestHelper::copyIntArray(arr1, n);
+  //
+  //    SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
+  //    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+  //
+  //    delete[] arr1;
+  //    delete[] arr2;// 测试2 测试近乎有序的数组
+  // 但是对于近乎有序的数组, 我们的快速排序算法退化成了O(n^2)级别的算法
+  // 思考一下为什么对于近乎有序的数组, 快排退化成了O(n^2)的算法? :)
+  int swapTimes = 100;
+  cout << "Test for nearly ordered array, size = " << n << ", swap time = " << swapTimes << endl;
+  arr1 = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
+  arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+  SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
+  SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+
+  delete[] arr1;
+  delete[] arr2;
+  cout << endl;
+
+  // 测试3 测试存在包含大量相同元素的数组
+  // 使用双快速排序后, 我们的快速排序算法可以轻松的处理包含大量元素的数组
+  cout << "Test for random array, size = " << n << ", random range [0,10]" << endl;
+  arr1 = SortTestHelper::generateRandomArray(n, 0, 10);
+  arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+  SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
+  SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+
+  delete[] arr1;
+  delete[] arr2;
+
+  return 0;
+}
